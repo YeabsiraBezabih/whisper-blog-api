0dee8fe617d1732f6994966c29e123d514137aff
yeabsirabezabih <yeabsiarbezabih791@gmail.com>
2026-07-02 14:46:13 +0300

    feat: initialize Prisma database schema and implement auth module scaffolding

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Post('login')
-  login() {
-    return '/login';
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
   }
 
   @Post('register')
-  register() {
-    return '/register';
+  register(@Body() registerDto: RegisterDto) {
+    return this.authService.register(registerDto);
   }
 
   @Post('refresh')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  login(loginDto: LoginDto) {
+    console.log(loginDto);
+    return 'login';
+  }
+
+  register(registerDto: RegisterDto) {
+    console.log(registerDto);
+    return 'register';
+  }
+}
